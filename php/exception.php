40a0898e964f92a2a7f109f942c4bd4089100dd5
RikuyaChiba <rikuya.chiba@gmail.com>
2025-02-21 23:24:00 +0900

    自己紹介もするようにした

--- php/exception.php
+++ php/exception.php
@@ -1,11 +1,34 @@
 <?php
 
 interface MajinBoo {
+
+    public function sayProfile();
+
     public function turnToChocolate($enemy);
 }
 
+class MajinBooEvil implements MajinBoo {
+
+    public function sayProfile()
+    {
+        echo "Ore ha Majin Boo da \n";
+    }
+
+    public function turnToChocolate($enemy)
+    {
+        echo "{$enemy}! Choco ni nare! \n";
+    }
+
+}
+
 class MajinBooGood implements MajinBoo {
+
     private const FRIEND = 'Mr. Satan';
+
+    public function sayProfile()
+    {
+        echo "Boku ha Majin Boo \n";
+    }
 
     public function turnToChocolate($enemy) 
     {
@@ -13,14 +36,14 @@ class MajinBooGood implements MajinBoo {
             if ($enemy === self::FRIEND) {
                 throw new Exception("I can't turn " . self::FRIEND . " into chocolate!", 404);
             }
-            echo "{$enemy}! Choco ni nacchae!";
+            echo "{$enemy}! Choco ni nacchae! \n";
         } catch (Exception $e) {
             echo 
                 "Message: " . $e->getMessage() . "\n" . 
                 "Code: " . $e->getCode() . "\n" .
                 "File: " . $e->getFile() . "\n" .
                 "Line: " . $e->getLine() . "\n" .
-                "Trace: " . $e->getTraceAsString();
+                "Trace: " . $e->getTraceAsString() . "\n";
         }
     }
 }
@@ -33,9 +56,15 @@ const enemies = [
     'Mr. Satan'
 ];
 
-$boo = new MajinBooGood();
+$boo_good = new MajinBooGood();
+$boo_evil = new MajinBooEvil();
 
+$boo_good->sayProfile();
 foreach (enemies as $enemy) {
-    $boo->turnToChocolate($enemy);
-    echo "\n";
+    $boo_good->turnToChocolate($enemy);
+}
+
+$boo_evil->sayProfile();
+foreach (enemies as $enemy) {
+    $boo_evil->turnToChocolate($enemy);
 }
