e2c406156cf9518d0c3b665dc573b5ca82da9e6c
RikuyaChiba <landlixer@gmail.com>
2025-02-23 14:50:05 +0900

    phpunit環境を作った

---
+++ php/mockery/src/Greeter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Greeter
+{
+    public function sayHello()
+    {
+        return 'hello';
+    }
+}
---
+++ php/mockery/tests/GreeterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use App\Greeter;
+
+class GreeterTest extends TestCase
+{
+    public function test_helloという文字列が返される()
+    {
+        $greeter = new Greeter;
+        $this->assertSame('hello', $greeter->sayHello());
+    }
+}
